8933a74b837707055c569d8aee8cbb39ad0ee31a
071yoon <yeonggilol@gmail.com>
2023-09-18 12:08:49 +0900

    fix: set boilerplate script to comment

--- src/content.ts
+++ src/content.ts
@@ -1,15 +1,15 @@
 // This file is injected as a content script
-console.log("Hello from content script!");
+// console.log("Hello from content script!");
 
 // will fix later
 // @ts-ignore
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   // If the received message has the expected format...
-  console.log("recved msg", msg);
-  if (msg.msg === "scroll") {
-    console.log("recved!!!");
-    // Call the specified callback, passing
-    // the web-page's DOM content as argument
-    sendResponse();
-  }
+  // console.log("recved msg", msg);
+  // if (msg.msg === "scroll") {
+  //   console.log("recved!!!");
+  //   // Call the specified callback, passing
+  //   // the web-page's DOM content as argument
+  //   sendResponse();
+  // }
 });
